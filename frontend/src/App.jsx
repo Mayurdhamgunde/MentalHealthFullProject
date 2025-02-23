ff94330fe2a60478583ff7f944e8d1c078ebfd35
mayur121-cmd <priyadhamagunde@gmail.com>
2025-02-23 21:15:16 +0530

    updated backend and removed Twilio

---
+++ frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './pages/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Login from './pages/Login';
+import FormContainer from './FormContainer';
+import Footer from './pages/Footer';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-amber-50/50 bg-gradient-to-b from-[rgb(241,232,255)] to-[rgb(228,222,254)]">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/predict" element={<FormContainer />} />
+          
+        </Routes>
+        {/* <FormContainer /> */}
+        <Footer />
+
+        
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
